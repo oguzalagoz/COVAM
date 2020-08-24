191d51370d0362150b6f3127317238baa24a760b
ay14904 <70037134+ay14904@users.noreply.github.com>
2020-08-24 16:42:43 -0500

    Add files via upload

---
+++ COVAM Runs and Data/stop-NYC/Covid_batch_2020_07_31_NYC-stop experiments.bat
@@ -0,0 +1,53 @@
+set seed=2000
+set exefile=CovidABM.exe
+set region=Dane_expected
+set num_rep=100
+set sim_day=365
+set act_adh=1
+set adh_level=0
+set sch_close=1
+set delay=0
+set stop_day=365
+set adh_stop=0.85
+
+set region=NYC
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set stop_day=276
+set adh_stop=0.85
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.8
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.75
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.7
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+
+set stop_day=269
+set adh_stop=0.85
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.8
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.75
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.7
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+
+set stop_day=262
+set adh_stop=0.85
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.8
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.75
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.7
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+
+set stop_day=246
+set adh_stop=0.85
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.8
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.75
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
+set adh_stop=0.7
+%exefile% %seed% %region% %num_rep% %sim_day% %act_adh%  %adh_level% %sch_close% %delay% %stop_day% %adh_stop%  out_%region%_stop%stop_day%_adhstop%adh_stop%.txt CI_%region%_stop%stop_day%_adhstop%adh_stop%.txt
